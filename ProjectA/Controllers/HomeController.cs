24993306bdb2fdb5cd9009d749e6b63c0dc4ffa3
Luis Carlos Noguera Maradiaga <lnoguera@unan.edu.ni>
2019-08-12 10:02:52 -0600

    Agregar archivos de proyecto.

---
+++ ProjectA/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectA.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var projectBUrl = System.Web.Configuration.WebConfigurationManager.AppSettings["ProjectBUrl"];
+            ViewBag.ProjectBUrl = projectBUrl;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ProjectB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectB.Startup))]
+namespace ProjectB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
